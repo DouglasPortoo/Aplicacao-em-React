99021e196a220271e411260a60a8bc5bc33cd949
Douglas <portogdouglas@gmail.com>
2023-09-09 14:29:36 -0300

    criando os estilos globais do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import theme from './styles/theme'
 
+import GlobalStyle from './styles/global'
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle/>
       <h1>oi</h1>
     </ThemeProvider>
   </React.StrictMode>
