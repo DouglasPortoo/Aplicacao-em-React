3287dd0e07c889e7f69be568d1abfd6ccbed1b6a
Douglas <portogdouglas@gmail.com>
2023-09-14 13:48:40 -0300

    adicionando/removendo/renderizando tags

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -5,8 +5,24 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { MovieItem } from "../../components/MovieItem";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function CreateMovie() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [rating, setRating] = useState("");
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  function handleAddTag() {
+    setTags((prevState) => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -18,21 +34,42 @@ export function CreateMovie() {
           <h1>Novo Filme</h1>
 
           <div className="input">
-            <Input placeholder="Titulo" />
-            <Input placeholder="Sua nota(de 0 a 5)" />
+            <Input
+              placeholder="Titulo"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <Input
+              placeholder="Sua nota(de 0 a 5)"
+              onChange={(e) => setRating(e.target.value)}
+            />
           </div>
 
           <textarea
             placeholder="Observações"
             cols="30"
             rows="10"
+            onChange={(e) => setDescription(e.target.value)}
           ></textarea>
 
           <h2>Marcadores</h2>
 
           <section>
-            <MovieItem placeholder="Novo marcador" isNew />
-            <MovieItem value="Drama" />
+          <MovieItem
+              placeholder="Novo marcador"
+              isNew
+              onChange={(e) => setNewTag(e.target.value)}
+              value={newTag}
+              onClick={handleAddTag}
+            />
+
+            {tags.map((tag, index) => (
+              <MovieItem
+                key={String(index)}
+                value={tag}
+                onClick={() => handleRemoveTag(tag)}
+              />
+            ))}
+            
           </section>
 
           <Button title="Salvar" />
