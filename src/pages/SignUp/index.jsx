b04e0e7b7b9e25226b5595402253265c2be358ac
Douglas <portogdouglas@gmail.com>
2023-09-14 09:22:54 -0300

    Cadastro de usuários

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,14 +1,45 @@
-import { Container, Form,Backgroud } from "./style";
+import { Container, Form, Backgroud } from "./style";
 import { Input } from "../../components/Input";
-import {Button} from "../../components/Button";
-import {ButtonText} from "../../components/ButtonText"
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaLock } from "react-icons/fa";
-import {BsFillPersonFill} from 'react-icons/bs'
-import { Link } from "react-router-dom";
+import { BsFillPersonFill } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { api } from "../../services/api";
 
 
 export function SingUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const usenavigate= useNavigate()
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
+
+   try {
+
+    api.post("/users", {name,email,password})
+    alert("Cadastrado com sucesso")
+
+    usenavigate("/")
+    
+   } catch (error) {
+    if (error.response) {
+      alert(error.response.data.message);
+    } else {
+      alert("Não foi possível cadastrar.");
+    }
+   }
+  }
+
   return (
     <Container>
       <Form>
@@ -17,30 +48,25 @@ export function SingUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input 
-        type="text" 
-        placeholder="Nome" 
-        icon={BsFillPersonFill}/>
+        <Input
+          type="text"
+          placeholder="Nome"
+          icon={BsFillPersonFill}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <Input 
-        type="email" 
-        placeholder="E-mail" 
-        icon={AiOutlineMail}/>
+        <Input type="email" placeholder="E-mail" icon={AiOutlineMail} onChange={(e) => setEmail(e.target.value)} />
 
-        <Input 
-        type="password" 
-        placeholder="Senha" 
-        icon={FaLock}/>
+        <Input type="password" placeholder="Senha" icon={FaLock} onChange={(e) => setPassword(e.target.value)} />
 
-        <Button title="Cadastrar"/>
-        
+        <Button title="Cadastrar" onClick={handleRegister} />
+
         <Link to="/">
-        <ButtonText title="Voltar para o login"/> 
-      </Link>
+          <ButtonText title="Voltar para o login" />
+        </Link>
       </Form>
 
-      <Backgroud/>
-      
+      <Backgroud />
     </Container>
   );
 }
